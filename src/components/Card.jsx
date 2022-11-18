022369931dd769b70d3ed0e29de4e91243ba50b0
Ethm KZLYR <106928233+Ethemkizilyer@users.noreply.github.com>
2022-11-18 21:05:34 +0300

    update

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -39,6 +39,33 @@ const Card = ({ item }) => {
     <>
       <div className="rounded-lg shadow-md  max-w-sm w-[350px] relative bg-gray-200 shadow-black mb-12">
         <div className="w-[90%] h-36">
+          <div className="w-[90%] h-36">
+            {item.ImgUrl ? (
+              <img
+                className="rounded-t-lg w-40 mx-auto"
+                src={item.ImgUrl}
+                alt=""
+              />
+            ) : (
+              <img className="rounded-t-lg w-48 mx-auto" src={konu} alt="" />
+            )}
+          </div>
+          <div className="p-6 flex flex-col justify-start">
+            <h5 className="text-gray-900 text-xl font-medium mb-2">
+              Card title
+            </h5>
+            <p className="text-gray-700 text-base mb-4">
+              This is a wider card with supporting text below as a natural
+              lead-in to additional content. This content is a little bit
+              longer.
+            </p>
+            <p className="text-gray-600 text-xs">Last updated 3 mins ago</p>
+          </div>
+        </div>
+      </div>
+
+      <div className="rounded-lg shadow-md  max-w-sm w-[350px] relative bg-gray-200 shadow-black mb-12">
+        <div className="w-[90%] h-36">
           {item.ImgUrl ? (
             <img
               className="rounded-t-lg w-40 mx-auto"
